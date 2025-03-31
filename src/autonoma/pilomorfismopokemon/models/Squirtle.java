f609d2590a6922e1e3125f32b9dbd27801b02473
Martin <martintabordaaristizabal@gmail.com>
2025-03-31 18:09:38 -0500

    Se comento Squirtle

--- src/autonoma/pilomorfismopokemon/models/Squirtle.java
+++ src/autonoma/pilomorfismopokemon/models/Squirtle.java
@@ -13,38 +13,45 @@ public class Squirtle extends Pokemon implements PokemonAgua{
     public Squirtle(int numPokedex, String nombre, float pesoPokemon, String temporada) {
         super(numPokedex, nombre, pesoPokemon, temporada);
     }
-
+    // Método abstracto para el atacarPlancaje
+    // @return String
     @Override
     public String atacarPlancaje() {
         return "soy Squitle y estoy atacando con plancaje";
     }
-
+    // Método abstracto para el atacarAraniazo
+    // @return String
     @Override
     public String atacarAraniazo() {
         return "soy Squitle y estoy atacando con araniazo";
     }
-
+    // Método abstracto para el atacarMordisco
+    // @return String
     @Override
     public String atacarMordisco() {
         return "soy Squitle y estoy atacando con mordisco";
     }
-
+    // Método abstracto para el ataque "Hidro Bomba"
+    // @return String
     @Override
     public String atacarHidroBomba() {
         return "soy Squitle y estoy atacando con Hidro bomba";
 
     }
-
+    // Método abstracto para el atacarPistolaAgua
+    // @return String
     @Override
     public String atacarPistolaAgua() {
         return "soy Squitle y estoy atacando con pistola agua";
     }
-
+    // Método abstracto para el atacarBurguja
+    // @return String
     @Override
     public String atacarBurguja() {
         return "soy Squitle y estoy atacando con burbuja ";
     }
-
+    // Método abstracto para el atacarHidroPulso
+    // @return String
     @Override
     public String atacarHidroPulso() {
         return "soy Squitle y estoy atacando con hidro pulso";
