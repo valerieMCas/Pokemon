182b13d874a3f80f5c7f93576453343185aa6a4b
Martin <martintabordaaristizabal@gmail.com>
2025-03-31 18:05:30 -0500

    Se comento PokemonFuego

--- src/autonoma/pilomorfismopokemon/models/PokemonFuego.java
+++ src/autonoma/pilomorfismopokemon/models/PokemonFuego.java
@@ -9,8 +9,14 @@ package autonoma.pilomorfismopokemon.models;
  * @author usuario
  */
 public interface PokemonFuego {
+    // Método abstracto para el atacarPunioFuego
+    // @return String
     public abstract String atacarPunioFuego();
+    // Método abstracto para el atacarAcusas
+    // @return String
     public abstract String atacarAcusas();
+    // Método abstracto para el atacarLanzaLlamas
+    // @return String
     public abstract String atacarLanzaLlamas();
     
 }
