e6c24014ada79021a52394571b3e79f56f0038fe
Martin <martintabordaaristizabal@gmail.com>
2025-03-31 17:50:46 -0500

    Se comento todos los metodos de Charmander

--- src/autonoma/pilomorfismopokemon/models/Charmander.java
+++ src/autonoma/pilomorfismopokemon/models/Charmander.java
@@ -13,32 +13,38 @@ public class Charmander extends Pokemon implements PokemonFuego{
     public Charmander(int numPokedex, String nombre, float pesoPokemon, String temporada) {
         super(numPokedex, nombre, pesoPokemon, temporada);
     }
-
+    //metodo abstracto atacarPlancaje
+    // @return String
     @Override
     public String atacarPlancaje() {
         return "soy Charmander y estoy atacando con con plataje";
     }
-
+    //metodo abstracto atacarAraniazo
+    // @return String
     @Override
     public String atacarAraniazo() {
         return "soy Charmander y estoy atacando con araniazo";
     }
-
+    //metodo abstracto atacarMordisco
+    // @return String
     @Override
     public String atacarMordisco() {
         return "soy Charmander y estoy atacando con mordisco ";
     }
-
+    //metodo abstracto atacarPunioFuego
+    // @return String
     @Override
     public String atacarPunioFuego() {
         return "soy Charmander y estoy atacando con punio fuego";
     }
-
+    //metodo abstracto atacarAcusas
+    // @return String
     @Override
     public String atacarAcusas() {
         return "soy Charmander y estoy atacando con acusas";
     }
-
+    //metodo abstracto atacarLanzaLlamas
+    // @return String
     @Override
     public String atacarLanzaLlamas() {
         return "soy Charmander y estoy atacando con lanza llamas";
