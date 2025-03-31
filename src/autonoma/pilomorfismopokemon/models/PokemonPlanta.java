0fd9a295f295f011ae7928707ab4dfb9a52f004b
Martin <martintabordaaristizabal@gmail.com>
2025-03-31 18:07:22 -0500

    Se comento pokemonPlanta

--- src/autonoma/pilomorfismopokemon/models/PokemonPlanta.java
+++ src/autonoma/pilomorfismopokemon/models/PokemonPlanta.java
@@ -11,8 +11,16 @@ package autonoma.pilomorfismopokemon.models;
 public interface PokemonPlanta {
     //////////////////////////////////////////////////////////
     //////////////metodos abstractos
+    // Método abstracto para el atacarParalizar
+    // @return String
     public abstract String atacarParalizar();
+    // Método abstracto para el atacarDrenaje
+    // @return String
     public abstract String atacarDrenaje();
+    // Método abstracto para el atacarHojaAfilada
+    // @return String
     public abstract String atacarHojaAfilada();
+    // Método abstracto para el atacarLatigoCepa
+    // @return String
     public abstract String atacarLatigoCepa();
 }
