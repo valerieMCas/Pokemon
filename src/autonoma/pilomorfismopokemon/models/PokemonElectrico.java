aac4853f9b52655a0c312dd60643464d96d17e3c
valerieMCas <valeriemorenocastaneda@gmail.com>
2025-03-31 17:49:48 -0500

    se comenta codigo de pikachu, pokemon y pokemon electrico

--- src/autonoma/pilomorfismopokemon/models/PokemonElectrico.java
+++ src/autonoma/pilomorfismopokemon/models/PokemonElectrico.java
@@ -9,7 +9,16 @@ package autonoma.pilomorfismopokemon.models;
  * @author usuario
  */
 public interface PokemonElectrico {
+    //////////////////////////////////////////////////////////////////////////////////
+    //// metodo de atacar con impactrueno 
+    //////////////@returna un string
     public abstract String atacarImpactruno();
+    //////////////////////////////////////////////////////////////////////////////////
+    //// metodo de atacar con punio trueno 
+    /////////@returna un string
     public abstract String atacarPunioTrueno();
+    //////////////////////////////////////////////////////////////////////////////////
+    //// metodo de atacar con rayo carga 
+    ////////////@returna un string
     public abstract String atacarRayoCarga();
 }
--- src/autonoma/polimorfismopokemon/main/PolimorfismoPokemon.java
+++ src/autonoma/polimorfismopokemon/main/PolimorfismoPokemon.java
@@ -4,6 +4,14 @@
  */
 package autonoma.polimorfismopokemon.main;
 
+import autonoma.pilomorfismopokemon.models.Bulbasaur;
+import autonoma.pilomorfismopokemon.models.Charmander;
+import autonoma.pilomorfismopokemon.models.Pikachu;
+import autonoma.pilomorfismopokemon.models.Pokemon;
+import autonoma.pilomorfismopokemon.models.PokemonAgua;
+import autonoma.pilomorfismopokemon.models.PokemonElectrico;
+import autonoma.pilomorfismopokemon.models.Squirtle;
+
 /**
  *
  * @author usuario
@@ -14,7 +22,16 @@ public class PolimorfismoPokemon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Charmander charmander = new Charmander (4, "Charmander",(float) 2.8,"Primera temporada");
+        Pokemon pokemonC= new Charmander (2,"Charmander", (float) 4.2,"Segunda temporada");
+        Pokemon pokemon =new Bulbasaur (8,"Bulbasaur", (float) 8,"Primera temporada");
+        PokemonAgua pokemonA = new Squirtle (14, "Squirtle", (float) 1.2,"Primera temporada");
+        PokemonElectrico pokemonE = new Pikachu (3, "Pikachu", (float) 5.6, "segunda temporada");
+        System.out.println (charmander.atacarAcusas()+ "\n") ;
+        System.out.println (pokemonC.atacarPlancaje()+ "\n") ;
+        System.out.println (pokemon.atacarMordisco()+ "\n");
+        System.out.println (pokemonA.atacarHidroBomba()+"\n");
+        System.out.println (pokemonE.atacarPunioTrueno()+ "\n");
     }
     
 }
